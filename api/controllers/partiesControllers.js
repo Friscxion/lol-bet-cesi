da601a0ae9e5d84e8718818097b5ff9fa7b32397
Friscxion <erwann.champion@outlook.fr>
2022-05-18 08:17:58 +0200

    React init

---
+++ api/controllers/partiesControllers.js
@@ -0,0 +1,18 @@
+const Realm=require('realm');
+const { UUID } = Realm.BSON;
+
+const index = (req,res)=>{
+    res.send("Hello bets")
+}
+
+const getAllParties = async (req,res) => {
+    const db = await require('../db/database');
+    db.write(()=>{
+        let parties=db.objects("Parties");
+        res.json(parties);
+    })
+}
+
+
+
+module.exports = {index,getAllParties}
--- api/routes/parties.js
+++ api/routes/parties.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const partiesControllers= require('../controllers/partiesControllers');
 
+router.get('/',partiesControllers.getAllParties)
 
 module.exports = router;
